ba1b6f87671b4b810dbed04ee9734ac9863b3bbf
QyperXit <67711154+QyperXit@users.noreply.github.com>
2024-02-12 16:44:18 +0000

    modal

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -5,13 +5,16 @@ import "../styles/main.css";
 
 const Modal = ({ onClick }) => {
   return (
-    <dialog open className="modal_box ">
-      <div className="rules_title">
-        <h3>RULES</h3>
-        <i className="fa-solid fa-xmark" onClick={onClick}></i>
-      </div>
-      <img src={imageRules} alt="rules modal" className="modal" />
-    </dialog>
+    <>
+      <dialog open className="modal_box ">
+        <div className="rules_title">
+          <h3>RULES</h3>
+          <i className="fa-solid fa-xmark" onClick={onClick}></i>
+        </div>
+        <img src={imageRules} alt="rules modal" className="modal" />
+      </dialog>
+      <div className="dialog-background"></div>
+    </>
   );
 };
 
